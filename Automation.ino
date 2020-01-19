ee21b8cd9de15b4bb90b1263bc9f40efc408ca61
Nilay Paul <52963703+49paunilay@users.noreply.github.com>
2020-01-19 12:12:26 +0530

    Update Automation.ino

--- Automation.ino
+++ Automation.ino
@@ -1,21 +1,25 @@
 int lamp = 11;
-int vent = 12;
+int fan = 12;
 int light = 7;
 int pc = 6;
+// using four variables for four items .//
 
 int received = 0;
+//to store the output of bluetooth module we are using received variable//
 int light_state = 0;
-int vent_state =0;
+int fan_state =0;
 int pc_state = 0;
 
+//These tree variables store the state of items//
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(lamp,OUTPUT);
-  pinMode(vent,OUTPUT);
+  pinMode(fan,OUTPUT);
   pinMode(light,OUTPUT);
   pinMode(pc,OUTPUT);
+  //Starting Serial communication at 9600 bits and setting up the pin modes//
   
 
 }
@@ -41,17 +45,17 @@ void loop() {
     light_state =0;
     received = 0;
   }
-   if(vent_state == 0 && received =='a')
+   if(fan_state == 0 && received =='a')
   {
     digitalWrite(light,HIGH);
-    vent_state = 1;
+    fan_state = 1;
     received = 0;
     
   }
-  if(vent_state ==1 && received =='a')
+  if(fan_state ==1 && received =='a')
   {
     digitalWrite(light,LOW);
-    vent_state =0;
+    fan_state =0;
     received = 0;
   }
    if(pc_state == 0 && received =='2')
